a8300cf5254d0531aca9f0f855ed9deb935e9cea
a27657060 <a27657060@gmail.com>
2023-07-10 14:55:46 +0800

    build: add static files & json for model

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//載入static files
+app.use(express.static('public'))
+
+//首頁route設定
 app.get('/', (req, res) => {
-  res.redirect('/restaurants')
+  res.redirect('/restaurants') //導向restaurants
 })
 
+//restaurants清單 route設定
 app.get('/restaurants', (req, res) => {
   res.send('restaurants listing')
 })
 
+//restaurants清單detail route設定
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   res.send(`read list is ${id}`)
