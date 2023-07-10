0578779e5d7c01cd9d634bb082e3001fccd478aa
a27657060 <a27657060@gmail.com>
2023-07-10 14:49:57 +0800

    build:route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
+  res.send('restaurants listing')
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`read list is ${id}`)
 })
 
 app.listen(port, () => {
